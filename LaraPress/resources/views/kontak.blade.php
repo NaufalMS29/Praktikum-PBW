456b732179ca229e73457a7f7dbc625c1ce08594
NaufalMS29 <naufalms2005@gmail.com>
2025-10-03 10:13:00 +0700

    Prak 2 - Instalasi, konfigurasi, dan Halaman Pertama Kita

---
+++ LaraPress/resources/views/kontak.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Kontak Kami - LaraPress</title>
+</head>
+
+<body>
+    <h1>Kontak LaraPress</h1>
+    <p>Nama : Naufal Maulana Saputra</p>
+    <p>NPM : 4523210083</p>
+    <p>Email : naufal4523083@univpancasila.ac.id</p>
+    <p>No.Hp : 082118768976</p>
+    <a href="/tentang-kami">Lihat Halaman Tentang Kami</a>
+    <br>
+    <a href="/">Kembali ke Halaman Utama</a>
+</body>
+
+</html>
